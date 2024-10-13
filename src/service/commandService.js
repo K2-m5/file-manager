a257fb012a056a8fc674435d71e1651d835e00ff
Pavel Svirid <pavel.svirid@dfmx.com>
2024-10-13 12:39:47 +0300

    feat: commands up, cd, exit, ls added

--- src/service/commandService.js
+++ src/service/commandService.js
@@ -1,12 +1,28 @@
+import { changeDirectory, goUp } from "../commands/changeDirectory.js";
+import { list } from "../commands/listFiles.js";
 import { getInitVariables } from "../utils/commands.js";
-import { rootDirName } from "../utils/dirname.js";
 
 const USERNAME_COMMAND = '--your_username'
 
-const commandService = (command) => {
-  switch (command) {
+const commandService = async (command) => {
+  const [cmd, ...args] = command.trim().split(' ');
+  
+  switch (cmd) {
     case 'pwd':
-      return console.log(rootDirName)
+      return console.log(process.cwd())
+    
+    case 'ls': 
+      return await list(args[0])
+    
+    case 'cd': 
+      return changeDirectory(args[0])
+
+    case 'up': 
+      return goUp()
+
+    case '.exit': 
+      byeMessage()
+      process.exit()
       
     default:
       console.log(`Received: ${command}`);
--- src/utils/dirname.js
+++ src/utils/dirname.js
@@ -1,4 +1,4 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-export const rootDirName = dirname(fileURLToPath(import.meta.url));
+export const rootDirName = dirname(fileURLToPath(import.meta.url), '..');
